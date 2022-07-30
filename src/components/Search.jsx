d71087d3cafd0d0762380ba1accf7f6bf07e5b70
kondra4 <kondra405@gmail.com>
2022-07-31 00:48:16 +0300

    add query parametrs in URL

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,31 +1,37 @@
-import { useCallback, useContext } from "react";
+import { useCallback, useState } from "react";
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
 import InputGroup from "react-bootstrap/InputGroup";
-import { LinkContainer } from "react-router-bootstrap";
-import { SearchContext } from "../context/SearchContext";
 import debounce from "lodash.debounce";
+import { useNavigate } from "react-router-dom";
 
 function Search() {
-  const { setSearchTitle } = useContext(SearchContext);
+  const navigate = useNavigate();
+  const [searchTitle, setSearchTitle] = useState("");
 
-  const onChange = (e) => {
+  const changeSearchInput = (e) => {
     setSearchTitle(e.target.value);
   };
-  const debounceOnChange = useCallback(debounce(onChange, 1000), []);
+  const debounceOnChange = useCallback(debounce(changeSearchInput, 1000), []);
+
+  const clickButtonSearch = () => {
+    navigate({ pathname: "/search", search: `search=${searchTitle}` });
+  };
 
   return (
     <>
       <InputGroup className="mt-3 mb-3">
-        <LinkContainer to="/search">
-          <Button variant="outline-secondary" id="button-addon1">
-            Search
-          </Button>
-        </LinkContainer>
+        <Button
+          onClick={clickButtonSearch}
+          variant="outline-secondary"
+          id="button-addon1"
+        >
+          Search
+        </Button>
         <Form.Control
+          onChange={debounceOnChange}
           aria-label="Example text with button addon"
           aria-describedby="basic-addon1"
-          onChange={debounceOnChange}
         />
       </InputGroup>
     </>
